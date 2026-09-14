62a2cc0c6353d1425e163da06637b34b102b16d9
LuisKabuto <115038299+LuisKabuto@users.noreply.github.com>
2026-09-14 12:07:16 -0400

    Conectar Firebase Authentication y Firestore

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 import { firebaseConfig } from "./firebase.js";
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export { onAuthStateChanged, signOut };
